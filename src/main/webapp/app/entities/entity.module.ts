4fb353683d7644d04bab4544cbd0cef628f9b732
jkaranja <jkaranja@meliora.tech>
2018-12-28 15:11:00 +0300

    regerate enities & changed to material scss

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { TwentyTwoPointsPointsModule } from './points/points.module';
+import { TwentyTwoPointsBloodPressureModule } from './blood-pressure/blood-pressure.module';
+import { TwentyTwoPointsWeightModule } from './weight/weight.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
         TwentyTwoPointsPointsModule,
+        TwentyTwoPointsBloodPressureModule,
+        TwentyTwoPointsWeightModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
